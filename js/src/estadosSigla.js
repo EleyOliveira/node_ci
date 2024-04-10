79d8f5aee52e42335f799f75a4a056c89e20cc2d
EleyOliveira <eleyoliveira@bol.com.br>
2024-04-10 08:26:11 -0300

    feat: inclusao de estados
    incluidos estados da região Sul

--- js/src/estadosSigla.js
+++ js/src/estadosSigla.js
@@ -1,9 +1,18 @@
 function consultaSiglaEstadoSudeste(estado) {
     const estadoSigla = new Map([
-        ["Sao Paulo", "SP"],
+        ["São Paulo", "SP"],
         ["Rio de Janeiro", "RJ"],
         ["Minas Gerais", "MG"],
         ["Espiríto Santo", "ES"]
+    ]);
+    return estadoSigla.get(estado);
+}
+
+function consultaSiglaEstadoSul(estado) {
+    const estadoSigla = new Map([
+        ["Paraná", "PR"],
+        ["Santa Catarina", "SC"],
+        ["Rio Grande do Sul", "RS"]        
     ]);
     return estadoSigla.get(estado);
 }
