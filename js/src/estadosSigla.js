ff397f56f4043951d963328d53e07063da5d865e
EleyOliveira <eleyoliveira@bol.com.br>
2024-04-11 15:18:31 -0300

    test: incluido registros de teste
    verificar se aparece porcentagem de coverage

--- js/src/estadosSigla.js
+++ js/src/estadosSigla.js
@@ -19,8 +19,33 @@ function consultaSiglaEstadoSul(estado) {
 
 function consultaSiglaEstadoCentroOeste(estado) {
     const estadoSigla = new Map([
-        ["Goiás", "GO"]
+        ["Goiás1", "GO"],
+        ["Goiás2", "GO"],
+        ["Goiás3", "GO"],
+        ["Goiás4", "GO"],
+        ["Goiás5", "GO"],
+        ["Goiás6", "GO"],
+        ["Goiás7", "GO"],
+        ["Goiás8", "GO"],
+        ["Goiás9", "GO"],
+        ["Goiás10", "GO"],
+        ["Goiás11", "GO"],
+        ["Goiás12", "GO"],
+        ["Goiás13", "GO"],
+        ["Goiás14", "GO"],
+        ["Goiás15", "GO"],
+        ["Goiás16", "GO"],
+        ["Goiás17", "GO"],
+        ["Goiás18", "GO"],
+        ["Goiás19", "GO"],
+        ["Goiás20", "GO"],
+        ["Goiás21", "GO"],
+        ["Goiás22", "GO"],
+        ["Goiás23", "GO"],
+        ["Goiás24", "GO"],
+        ["Goiás25", "GO"]
     ])
+    return estadoSigla.get(estado)
 }
 
 module.exports = consultaSiglaEstadoSudeste;
